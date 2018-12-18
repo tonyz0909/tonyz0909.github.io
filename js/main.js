bed92ea64ad5fbf755048f0949fb36536bcb283a
tonyz0909 <tonyz0909@gmail.com>
2018-12-18 18:20:40 -0500

    content/layout updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$('#scroll').on('click', function() {
+$('#scroll').on('click', function () {
   $("html, body").animate({ scrollTop: $($(this).attr('href')).offset().top }, 500); //$("#elementtoScrollToID").offset().top
 })
+
+$('#cat1').on('click', function () {
+    $("#skills1").css("width", "100%")
+})
+
+$('#cat2').on('click', function () {
+  $("#skills2").css("width", "100%")
+})
+
