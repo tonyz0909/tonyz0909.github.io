2a232bf5754b2f0b05fa7e613601ca63e300af69
tonyz0909 <tonyz0909@gmail.com>
2020-03-16 12:42:27 -0400

    added mobile responsive navbar

--- js/main.js
+++ js/main.js
@@ -10,3 +10,43 @@ $('#cat2').on('click', function () {
   $("#skills2").css("width", "100%")
 })
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+/* sticky navbar */
+
+var navbar = document.getElementById("navbar");
+var wrapper = document.getElementById("wrapper");
+
+window.onload = () => {
+  navbar.style.display = 'none';
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.style.display = 'block';
+    navbar.classList.add("sticky");
+  } else {
+    navbar.style.display = 'none';
+    navbar.classList.remove("sticky");
+  }
+}
+
