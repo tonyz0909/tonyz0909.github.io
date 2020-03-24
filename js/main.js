b0490a28f493e741055a3e4e3e8b9e0b65f27b6e
tonyz0909 <tonyz0909@gmail.com>
2020-03-24 17:46:20 -0400

    cleanup

--- js/main.js
+++ js/main.js
@@ -35,27 +35,3 @@ toggleNavbar = () => {
   }
 }
 
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-
-//     /* Check the location of each desired element */
-//     $('.body_section').each(function (i) {
-//       console.log($(this).position().top + $(this).outerHeight());
-//       console.log($(window).scrollTop() + $(window).height())
-//       var bottom_of_object = $(this).position().top + $(this).outerHeight();
-//       var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-//       /* If the object is completely visible in the window, fade it it */
-//       if (bottom_of_window > bottom_of_object) {
-
-//         $(this).animate({
-//           'opacity': '1'
-//         }, 2000);
-
-//       }
-
-//     });
-
-//   });
-// });
-
